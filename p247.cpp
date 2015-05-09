04d359d70c9042a6bddbf0b42f62191dd1f2cfce
selat <selatnick@gmail.com>
2015-05-09 13:34:30 +0300

    Improve solution to the problem 247

--- p247.cpp
+++ p247.cpp
@@ -3,14 +3,24 @@
 
 using namespace std;
 
+struct State
+{
+	State() :
+		min_money(1000000),
+		used_coupons(0)
+	{
+	}
+	int min_money;
+	int used_coupons;
+};
+
 int main()
 {
 	int n;
 	cin >> n;
-	vector <int> a(n);
-	vector <vector <int> > d(n ,vector <int>(n, 100000));
-	vector <vector <int> > coupons_num(n, vector <int>(n, 0));
-	for(int i = 0; i < n; ++i) {
+	vector <int> a(n + 1);
+	vector <vector <State> > d(n + 1 ,vector <State>(n + 1));
+	for(int i = 1; i <= n; ++i) {
 		cin >> a[i];
 	}
 	if(n == 0) {
@@ -18,48 +28,41 @@ int main()
 		cout << "0 0" << endl;
 		return 0;
 	}
-	d[0][0] = a[0];
-	coupons_num[0][0] = ((a[0] > 100)?1:0);
-	for(int i = 1; i < n; ++i) {
-		d[i][0] = d[i - 1][0] + a[i];
-		coupons_num[i][0] = coupons_num[i - 1][0] + ((a[i] > 100)?1:0);
-		cout << d[i][0] << " " << i << " " << 0 << endl;
-		for(int j = 1; j < n; ++j) {
-			d[i][j] = d[i - 1][j] + a[i];
-			coupons_num[i][j] = coupons_num[i - 1][j] + ((a[i] > 100)?1:0);
-			cout << d[i][j] << " ";
-			int sum = 0;
-			for(int k = i; k >= 1; --k) {
-				if(coupons_num[k - 1][j - 1] >= j) {
-					if(d[k - 1][j - 1] + sum < d[i][j]) {
-						d[i][j] = d[k - 1][j - 1] + sum;
-						cout << "I: " << d[i][j] << " " << k << endl;
-						coupons_num[i][j] = coupons_num[k - 1][j - 1];
-					} else if(d[k - 1][j - 1] + sum == d[i][j]) {
-						coupons_num[i][j] = max(coupons_num[i][j], coupons_num[k - 1][j - 1]);
-					}
-				}
-				sum += a[k];
+	d[0][0].min_money = 0;
+	d[0][0].used_coupons = 0;
+	/*
+	 * d[day][rem_coupons].min_money = min(
+	 *     d[day - 1][rem_coupons - 1].min_money + a[day], here we buy lunch at current day, and it's cost is bigger than 100, therefore we get one additional coupon.
+	 *     d[day - 1][rem_coupons].min_money + a[day], here we buy lunch at current day, but it's cost isn't bigger than 100, therefore we don't get one additional couopon.
+	 *     d[day - 1][rem_coupons + 1].min_money, here we use on of coupons to get free lunch for current day.
+	 * )
+	 */
+	for(int day = 1; day <= n; ++day) {
+		for(int rem_coupons = 0; rem_coupons <= n; ++rem_coupons) {
+			if((rem_coupons - 1 >= 0) && (a[day] > 100) && (d[day - 1][rem_coupons - 1].min_money + a[day] < d[day][rem_coupons].min_money)) {
+				d[day][rem_coupons].min_money = d[day - 1][rem_coupons - 1].min_money + a[day];
+				d[day][rem_coupons].used_coupons = d[day - 1][rem_coupons - 1].used_coupons;
 			}
-			cout << d[i][j] << " " << i << " " << j << endl;
-		}
-	}
-	int ans = 100000;
-	int left_coupons = 0;
-	int spent_coupons = 0;
-	for(int i = 0; i < n; ++i) {
-		if(d[n - 1][i] < ans) {
-			ans = d[n - 1][i];
-			left_coupons = coupons_num[n - 1][i] - i;
-			spent_coupons = i;
-		} else if(d[n - 1][i] == ans) {
-			if(coupons_num[n - 1][i] - i > left_coupons) {
-				left_coupons = coupons_num[n - 1][i] - i;
-				spent_coupons = i;
+			if((a[day] <= 100) && (d[day - 1][rem_coupons].min_money + a[day] < d[day][rem_coupons].min_money)) {
+				d[day][rem_coupons].min_money = d[day - 1][rem_coupons].min_money + a[day];
+				d[day][rem_coupons].used_coupons = d[day - 1][rem_coupons].used_coupons;
+			}
+			if((rem_coupons + 1 < n) && (d[day - 1][rem_coupons + 1].min_money < d[day][rem_coupons].min_money)) {
+				d[day][rem_coupons].min_money = d[day - 1][rem_coupons + 1].min_money;
+				d[day][rem_coupons].used_coupons = d[day - 1][rem_coupons + 1].used_coupons;
+				++d[day][rem_coupons].used_coupons;
 			}
 		}
 	}
-	cout << ans << endl;
-	cout << left_coupons << " " << spent_coupons << endl;
+	State best_state;
+	int best_rem_coupons = 0;
+	for(int rem_coupons = 0; rem_coupons <= n; ++rem_coupons) {
+		if(d[n][rem_coupons].min_money <= best_state.min_money) {
+			best_state = d[n][rem_coupons];
+			best_rem_coupons = rem_coupons;
+		}
+	}
+	cout << best_state.min_money << endl;
+	cout << best_rem_coupons << " " << best_state.used_coupons << endl;
 	return 0;
 }
